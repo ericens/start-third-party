7cd00bbc7258355fd24ec7e8e2a34f408fff30b1
ericens <zenglx@jpush.cn>
2019-08-11 21:24:10 +0800

    mysql

---
+++ gradle-guide/src/main/java/org.zlx/qrCode/Main.java
@@ -0,0 +1,37 @@
+package org.zlx.qrCode;
+
+/**
+ * Created by @author linxin on 20/10/2017.  <br>
+ */
+
+import net.glxn.qrgen.image.ImageType;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String str="http://www.cnblogs.com/xz-luckydog/p/6402568.html";
+        String url="hello world";
+        generateRCCode(str,"/Users/ericens/downloads/1.JPG");
+        generateRCCode(url,"/Users/ericens/downloads/2.JPG");
+
+    }
+
+    public static void generateRCCode( String str,String path) {
+        ByteArrayOutputStream out = net.glxn.qrgen.QRCode.from(str).to(ImageType.PNG).stream();
+
+        try {
+            FileOutputStream fout = new FileOutputStream(new File(
+                    path));
+            fout.write(out.toByteArray());
+
+            fout.flush();
+            fout.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
